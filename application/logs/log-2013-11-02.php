73e8519117b26f38ebc22d17377088cbd3c8559b
alimulrazi <alimulrazi28@gmail.com>
2013-11-02 12:58:54 +0600

    Add git ignore file

---
+++ application/logs/log-2013-11-02.php
@@ -0,0 +1,4 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2013-11-02 06:57:47 --> 404 Page Not Found --> 
+ERROR - 2013-11-02 06:57:47 --> 404 Page Not Found --> 
