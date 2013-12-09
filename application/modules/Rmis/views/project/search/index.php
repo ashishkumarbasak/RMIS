d7e2f1b953c0d7e3baedb50355318d5c166154c7
Ashish Kumar Basak <ashish021@gmail.com>
2013-12-10 00:57:33 +0600

    add missing project search index file

---
+++ application/modules/Rmis/views/project/search/index.php
@@ -0,0 +1,28 @@
+<div class="content-header">
+  <h2>
+  	<i <?php echo $content_header_icon; ?>></i> 
+  	<?php echo $content_header_title; ?>
+  </h2>
+</div>
+
+<!-- content-breadcrumb -->
+<div class="content-breadcrumb"> 
+  <?php echo $breadcrumb; ?>
+</div>
+<!-- /content-breadcrumb --> 
+
+<!-- content-body -->
+<div class="content-body">    
+    <div class="row-fluid">
+        <?php echo $template['partials']['divInfoForm']; ?>
+        <div style="margin:15px;">
+        	<div class="row-fluid">
+           		<?php //echo $grid_data; ?>
+           		<div id="result"></div>
+        	</div>
+       	</div>
+    </div> 
+</div><!--/content-body -->
+<div style="height:10px;"></div>
+<script id="popup_editor" type="text/x-kendo-template">
+</script>
